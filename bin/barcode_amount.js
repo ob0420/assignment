7117bc1bd49bbbdb7f486fa4fb10b293a1018a26
ob0420 <ogulgerek.babakulyyeva@bil.omu.edu.tr>
2021-08-14 02:12:19 +0300

    barcode_amount report updated

--- bin/barcode_amount.js
+++ bin/barcode_amount.js
@@ -1,5 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const { Query } = require("mongoose");
+const fs = require("fs")
 
 const uri = "mongodb://127.0.0.1:27017"; // Connection setups for Mongodb installed on local machine
 const client = new MongoClient(uri);
@@ -8,10 +9,14 @@ MongoClient.connect(uri, function(err, db) {
   if (err) throw err;
   var dbo = db.db("Items"); // Selected Items database, inside it counts and master inputs are imported into collections
   //var query = {locationCode: "D-117"};
-  dbo.collection("counts").find({}).project({barcode:1, amount:1, _id:0}).toArray(function(err, result) {  //Here I want to select only barcode and amount field inside nested document, 
-                                                                                                           //I cant access sub fields for now, but working on it.
+  dbo.collection("counts").find({}).project({"completedCounts.contents.barcode":1, "completedCounts.contents.amount":1, _id:0}).toArray(function(err, result) {  //Here I am selecting only barcode and amount field, nested inside document
+                                                                                                           
     if (err) throw err;
     console.log(result);
+    fs.writeFile("result.txt", JSON.stringify(result).replace(/]|[[]/g, ''), err => {
+      if (err) throw err;
+      console.log('File successfully written to disk');
+ })
     db.close();
   });
 });
